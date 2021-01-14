ef213f012b6c1eb6b79876d75b38e7fa6c813be3
Alen Thomas Nellary <alenthomas678@gmail.com>
2021-01-14 23:19:44 +0530

    final commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const http = require('http');
 const app = express();
-const server = http.createServer(app);
+const server = http.Server(app);
 const socket = require('socket.io');
 const io = socket(server, {
   path: '/io/webrtc',
