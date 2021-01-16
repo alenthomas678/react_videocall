220bb6d3e0ef672daf9d38266ed83f00998d78b6
Alen Thomas Nellary <alenthomas678@gmail.com>
2021-01-16 22:45:39 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -20,40 +20,40 @@ class App extends Component {
             pc_config: {
                 "iceServers": [
                     {
-                        url: 'turn:global.turn.twilio.com:3478?transport=udp',
-                        username: '08d8032f0fbfb6bd01acdb6a78ef56ea08150468c92514c10bfa788536465808',
-                        urls: 'turn:global.turn.twilio.com:3478?transport=udp',
-                        credential: 'kBZTE33+2x3/qZYbE5edOlowo2grOZwizv1iV7noq7E='
+                        'url': 'turn:global.turn.twilio.com:3478?transport=udp',
+                        'username': '08d8032f0fbfb6bd01acdb6a78ef56ea08150468c92514c10bfa788536465808',
+                        'urls': 'turn:global.turn.twilio.com:3478?transport=udp',
+                        'credential': 'kBZTE33+2x3/qZYbE5edOlowo2grOZwizv1iV7noq7E='
                     },
                     {
-                        url: 'turn:global.turn.twilio.com:3478?transport=tcp',
-                        username: '08d8032f0fbfb6bd01acdb6a78ef56ea08150468c92514c10bfa788536465808',
-                        urls: 'turn:global.turn.twilio.com:3478?transport=tcp',
-                        credential: 'kBZTE33+2x3/qZYbE5edOlowo2grOZwizv1iV7noq7E='
+                        'url': 'turn:global.turn.twilio.com:3478?transport=tcp',
+                        'username': '08d8032f0fbfb6bd01acdb6a78ef56ea08150468c92514c10bfa788536465808',
+                        'urls': 'turn:global.turn.twilio.com:3478?transport=tcp',
+                        'credential': 'kBZTE33+2x3/qZYbE5edOlowo2grOZwizv1iV7noq7E='
                     },
                     {
-                        url: 'turn:global.turn.twilio.com:443?transport=tcp',
-                        username: '08d8032f0fbfb6bd01acdb6a78ef56ea08150468c92514c10bfa788536465808',
-                        urls: 'turn:global.turn.twilio.com:443?transport=tcp',
-                        credential: 'kBZTE33+2x3/qZYbE5edOlowo2grOZwizv1iV7noq7E='
+                        'url': 'turn:global.turn.twilio.com:443?transport=tcp',
+                        'username': '08d8032f0fbfb6bd01acdb6a78ef56ea08150468c92514c10bfa788536465808',
+                        'urls': 'turn:global.turn.twilio.com:443?transport=tcp',
+                        'credential': 'kBZTE33+2x3/qZYbE5edOlowo2grOZwizv1iV7noq7E='
                     },
                     {
-                        urls: ["turn:numb.viagenie.ca"],
-                        username: "alenthomas678@gmail.com",
-                        credential: "dio2063@benedire"
+                        'urls': ["turn:numb.viagenie.ca"],
+                        'username': "alenthomas678@gmail.com",
+                        'credential': "dio2063@benedire"
                     },
                     {
-                        url: 'stun:global.stun.twilio.com:3478?transport=udp',
-                        urls: 'stun:global.stun.twilio.com:3478?transport=udp',
-                        urls: 'stun:stun.l.google.com:19302'
+                        'url': 'stun:global.stun.twilio.com:3478?transport=udp',
+                        'urls': 'stun:global.stun.twilio.com:3478?transport=udp',
+                        'urls': 'stun:stun.l.google.com:19302'
                     },
                     {
-                        urls: "stun:stun.stunprotocol.org"
+                        'urls': "stun:stun.stunprotocol.org"
                     },
                     {
-                        urls: 'turn:numb.viagenie.ca',
-                        credential: 'muazkh',
-                        username: 'webrtc@live.com'
+                        'urls': 'turn:numb.viagenie.ca',
+                        'credential': 'muazkh',
+                        'username': 'webrtc@live.com'
                     },
                 ]
             },
